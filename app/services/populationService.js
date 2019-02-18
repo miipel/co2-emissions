78c558f83743f9ad73aede3e59c81a68c2c47b80
miipel <mpeltotalo@gmail.com>
2019-02-18 14:04:24 +0200

    Implemented a working pipe from API-endpoint to xml-string

--- app/services/populationService.js
+++ app/services/populationService.js
@@ -1,36 +1,22 @@
 const fetch = require('node-fetch')
 const streams = require('memory-streams')
 const unzip = require('unzip')
-const xml = require('xml-parse')
-const fs = require('fs')
 const baseUrl = 'http://api.worldbank.org/v2/en/indicator/SP.POP.TOTL?downloadformat=xml'
 
-
-const { Transform } = require('stream')
-
-const xmlParser = new Transform({
-  readableObjectMode: true,
-
-  transform(chunk, encoding, callback) {
-    this.push(xml.parse(chunk))
-    callback()
-  }
-})
-
-try {
-  fetch(baseUrl)
-    .then(response => response.body)
-    .then(body => {
-      const stream = body
-      stream
-        .pipe(unzip.Parse())
-        .pipe(xmlParser)
-      const writer = new streams.WritableStream()
-      stream.pipe(writer)
-      stream.on('readable', function() {
-        console.log(writer.toString())
+fetch(baseUrl)
+  .then(response => response.body)
+  .then(body => {
+    const stream = body
+    stream
+      .pipe(unzip.Parse())
+      .on('entry', entry => {
+        const writer = new streams.WritableStream()
+        entry.pipe(writer)
+        entry.on('end', () => {
+          console.log(writer.toString())
+        })
       })
-    })
-} catch(error) {
-  console.log(error)
-}
+  })
+  .catch(error => {
+    console.log(error)
+  })
